b6a9964b548f60f855c21b67ce867a5066a94d1c
Emre İnan <emreinannn@gmail.com>
2024-10-14 21:39:22 +0300

    ProfectController CRUD implemented.

---
+++ App.Shared/Dto/Project/ProjectDto.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Shared.Dto.Project;
+
+public class ProjectDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
+public class ProjectDtoValidator : AbstractValidator<ProjectDto>
+{
+    public ProjectDtoValidator()
+    {
+        RuleFor(x => x.Title).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.ImageUrl).NotEmpty();
+    }
+}
